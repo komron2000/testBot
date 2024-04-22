65f5dfbf5d934c509997cc99574f781cf683e9c3
komron2000 <rayimboevk@gmail.com>
2024-04-23 00:20:13 +0300

    7

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def main(message):
 def main(message):
     bot.send_message(message.chat.id, 'Привет! На Лунпу')
 
-bot.polling(non_stop=True)  # kihohoi
+bot.polling(non_stop=True)  # kihohoirtweg
