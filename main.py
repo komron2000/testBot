743076054c8fc31c4b223e56c1c5ce4a6fc61923
komron2000 <rayimboevk@gmail.com>
2024-04-22 23:28:13 +0300

    frist

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ bot = telebot.TeleBot('7161237552:AAFwmzqnQn-agiq5s_VXUDWN9qwPP5gASCw')
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.message_handlers(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, 'Привет!')
+
+
+bot.polling(non_stop=True)
