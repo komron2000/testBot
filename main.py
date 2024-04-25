b57163727be7546bf0eb57629b2f4b12568500c4
komron2000 <rayimboevk@gmail.com>
2024-04-25 22:42:01 +0300

    комент

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 
 bot = telebot.TeleBot('7161237552:AAFwmzqnQn-agiq5s_VXUDWN9qwPP5gASCw')
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start']) #psd
 def main(message):
     bot.send_message(message.chat.id, 'Привет! Ван Цзыхао')
 
